8f03bfd4a1678a2e000124b92b7a0467bc6c15c1
vipin14119 <vipin14119@iiitd.ac.in>
2017-02-26 23:34:39 +0530

    terminal background css creating

--- terminal.js
+++ terminal.js
@@ -1,8 +1,12 @@
 var linesQueue = [];
 function initTerminal(){
     var target = document.getElementById("terminal");
+    target.className += " terminal";
     // alert(target.innerHTML);
     populateIntoLineQueue(target);
+}
+function getImportHtml(text){
+    return '<span class="import">'+text+'</span>';
 }
 function populateIntoLineQueue(target){
     var html = String(target.innerHTML).trim();
@@ -11,10 +15,15 @@ function populateIntoLineQueue(target){
     var end = start;
     while(end != -1){
         end = html.indexOf("\n");
-        linesQueue.push(html.slice(0, end));
+        linesQueue.push(getImportHtml(html.slice(0, end)));
         html = html.substr(end + 1, html.length).trim();
     }
     console.log(linesQueue);
+    var temp = '';
+    for(var i=0;i<linesQueue.length;i++){
+        temp += linesQueue[i];
+    }
+    target.innerHTML = temp;
     // console.log(html);
     // alert("Length = "+html.length+" substr = "+html.substr(0, 10));
 }
