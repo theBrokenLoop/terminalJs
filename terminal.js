bf199ea489e96c073531e0794f6737d540c1088f
vipin14119 <vipin14119@iiitd.ac.in>
2017-02-27 00:13:58 +0530

    regex pattern not working correctly

--- terminal.js
+++ terminal.js
@@ -1,12 +1,41 @@
 var linesQueue = [];
 function initTerminal(){
     var target = document.getElementById("terminal");
+    /// Add terminal class for DOM
     target.className += " terminal";
-    // alert(target.innerHTML);
     populateIntoLineQueue(target);
 }
 function getImportHtml(text){
     return '<span class="import">'+text+'</span>';
+}
+function getMethodHtml(text){
+    return '<span class="method">'+text+'</span>';
+}
+function getStringHtml(text){
+    return '<span class="string">'+text+'</span>';
+}
+function replacer(match, p1, offset, string) {
+  // p1 is nondigits, p2 digits, and p3 non-alphanumerics
+    alert("Came in repalcer");
+  return getMethodHtml(p1);
+}
+function processLine(line){
+    /// Some preliminary operations on line
+    line.trim().replace("  ", "").replace(" ", "");
+    console.log("got string = "+line);
+    if (line.search(/(.\w+\(])/)){
+        alert("Found");
+        line = line.replace(/(.\w+\()]/, replacer);
+    }
+
+    if (line.indexOf("import") >= 0){
+        console.log("Found import");
+        line = line.replace("import", getImportHtml("import"));
+    }
+
+    // if (line.search(/.[\w]*/))
+    console.log("Returning = "+line);
+    return line;
 }
 function populateIntoLineQueue(target){
     var html = String(target.innerHTML).trim();
@@ -15,15 +44,14 @@ function populateIntoLineQueue(target){
     var end = start;
     while(end != -1){
         end = html.indexOf("\n");
-        linesQueue.push(getImportHtml(html.slice(0, end)));
+        linesQueue.push(html.slice(0, end));
         html = html.substr(end + 1, html.length).trim();
     }
     console.log(linesQueue);
     var temp = '';
     for(var i=0;i<linesQueue.length;i++){
-        temp += linesQueue[i];
+        temp += processLine(linesQueue[i]);
     }
+    console.log(temp);
     target.innerHTML = temp;
-    // console.log(html);
-    // alert("Length = "+html.length+" substr = "+html.substr(0, 10));
 }
