b37c5712c4c370d08c3fdc44ac0e6c0af23560b1
vipin14119 <vipin14119@iiitd.ac.in>
2017-02-26 23:23:31 +0530

    linesQueues populated by '\n' character

--- terminal.js
+++ terminal.js
@@ -1,4 +1,20 @@
+var linesQueue = [];
 function initTerminal(){
-    alert("Hello");
-    var target = document.
+    var target = document.getElementById("terminal");
+    // alert(target.innerHTML);
+    populateIntoLineQueue(target);
+}
+function populateIntoLineQueue(target){
+    var html = String(target.innerHTML).trim();
+    var start = html.search(/\w/);
+    alert("First char starts at = "+start);
+    var end = start;
+    while(end != -1){
+        end = html.indexOf("\n");
+        linesQueue.push(html.slice(0, end));
+        html = html.substr(end + 1, html.length).trim();
+    }
+    console.log(linesQueue);
+    // console.log(html);
+    // alert("Length = "+html.length+" substr = "+html.substr(0, 10));
 }
